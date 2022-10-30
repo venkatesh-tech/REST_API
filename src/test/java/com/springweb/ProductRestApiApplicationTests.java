ea660d86f465c20d8e7f33c7d2ba9a9439e555ac
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-30 18:24:51 +0530

    Added RestTemplate Operations

--- src/test/java/com/springweb/ProductRestApiApplicationTests.java
+++ src/test/java/com/springweb/ProductRestApiApplicationTests.java
@@ -15,9 +15,27 @@ class ProductRestApiApplicationTests {
 	@Test
 	void testGetProduct() {
 		RestTemplate restTemplate = new RestTemplate();
-		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/4", Product.class);
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/4", Product.class); //url and Response type
 		assertNotNull(product);
 		assertEquals("Hp laptop", product.getName());
+	}
+	
+	@Test
+	void testCreateProduct() {
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = new Product();
+		product.setName("Samsung");
+		product.setDescription("Awesome");
+		product.setPrice(250000);
+		Product newProduct = restTemplate.postForObject("http://localhost:8080/productapi/products/", product, Product.class); //url, product to be added, responseType
+		assertEquals("Samsung", newProduct.getName());
+	}
+	
+	void testUpdateProduct() {
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/", Product.class);
+		product.setPrice(1400);
+		restTemplate.put("http://localhost:8080/productapi/products/", product);
 	}
 
 }
