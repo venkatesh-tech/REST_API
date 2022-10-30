4fc770a1e3a8d59795cef158bf68a6c7ed5d55a9
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-30 14:46:15 +0530

    Changed web server

--- src/main/java/com/springweb/controller/ProductRestController.java
+++ src/main/java/com/springweb/controller/ProductRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,13 +34,13 @@ public class ProductRestController {
 	
 	//Creating a product
 	@RequestMapping(value="/products/", method = RequestMethod.POST)
-	public Product createProduct(Product product) {
+	public Product createProduct(@RequestBody Product product) {
 		return repository.save(product);
 	}
 	
 	//Updating the product
 	@RequestMapping(value="/products/",method=RequestMethod.PUT)
-	public Product updateProduct(Product product) {
+	public Product updateProduct(@RequestBody Product product) {
 		return repository.save(product);
 	}
 	
