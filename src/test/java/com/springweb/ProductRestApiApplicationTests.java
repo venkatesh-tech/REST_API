3451ceaae90e7a73bf0fe7e191096a8424d3282d
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-30 17:09:21 +0530

    Added RestTemplate Get

--- src/test/java/com/springweb/ProductRestApiApplicationTests.java
+++ src/test/java/com/springweb/ProductRestApiApplicationTests.java
@@ -1,13 +1,23 @@
 package com.springweb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+import com.springweb.entities.Product;
 
 @SpringBootTest
 class ProductRestApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testGetProduct() {
+		RestTemplate restTemplate = new RestTemplate();
+		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/4", Product.class);
+		assertNotNull(product);
+		assertEquals("Hp laptop", product.getName());
 	}
 
 }
