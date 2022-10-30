41fdb4a3c7951159de4c0f1601d52b3712f31e0b
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-30 10:14:06 +0530

    Added findById()

--- src/main/java/com/springweb/controller/ProductRestController.java
+++ src/main/java/com/springweb/controller/ProductRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springweb.entities.Product;
@@ -15,9 +18,14 @@ public class ProductRestController {
 	@Autowired
 	ProductRepository repository;
 	
-	@GetMapping("/products/")
+//	@GetMapping("/products/")
+	@RequestMapping(value="/products/", method = RequestMethod.GET)
 	public List<Product> getProducts(){
-		return repository.findAll();
-		
+		return repository.findAll();	
+	}
+	
+	@RequestMapping(value="/products/{id}", method = RequestMethod.GET)
+	public Product getProduct(@PathVariable("id") int id) {
+		return repository.findById(id).get();	
 	}
 }
