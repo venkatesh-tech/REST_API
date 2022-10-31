505a5bdacd58a6c0561fb2a4534ea109aaa28ca7
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-10-31 08:56:21 +0530

    Added @Value

--- src/test/java/com/springweb/ProductRestApiApplicationTests.java
+++ src/test/java/com/springweb/ProductRestApiApplicationTests.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,10 +13,14 @@ import com.springweb.entities.Product;
 @SpringBootTest
 class ProductRestApiApplicationTests {
 
+	@Value("${product_rest_api.services.url}")
+	private String baseURL;
+	 
+	
 	@Test
 	void testGetProduct() {
 		RestTemplate restTemplate = new RestTemplate();
-		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/4", Product.class); //url and Response type
+		Product product = restTemplate.getForObject(baseURL+"4", Product.class); //url and Response type
 		assertNotNull(product);
 		assertEquals("Hp laptop", product.getName());
 	}
@@ -27,15 +32,15 @@ class ProductRestApiApplicationTests {
 		product.setName("Samsung");
 		product.setDescription("Awesome");
 		product.setPrice(250000);
-		Product newProduct = restTemplate.postForObject("http://localhost:8080/productapi/products/", product, Product.class); //url, product to be added, responseType
+		Product newProduct = restTemplate.postForObject(baseURL, product, Product.class); //url, product to be added, responseType
 		assertEquals("Samsung", newProduct.getName());
 	}
 	
 	void testUpdateProduct() {
 		RestTemplate restTemplate = new RestTemplate();
-		Product product = restTemplate.getForObject("http://localhost:8080/productapi/products/", Product.class);
+		Product product = restTemplate.getForObject(baseURL+"2", Product.class);
 		product.setPrice(1400);
-		restTemplate.put("http://localhost:8080/productapi/products/", product);
+ 		restTemplate.put("http://localhost:8080/productapi/products/", product);
 	}
 
 }
