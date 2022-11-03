b5a576d6b22fa974cb5a634aeb8adbe4a01c5b0d
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-11-03 19:19:16 +0530

    Added Swagger & Report Bug

--- src/main/java/com/springweb/ProductRestApiApplication.java
+++ src/main/java/com/springweb/ProductRestApiApplication.java
@@ -3,9 +3,16 @@ package com.springweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 @EnableCaching
+@EnableSwagger2
 public class ProductRestApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springweb/healthchecks/CustomHealthIndicator.java
+++ src/main/java/com/springweb/healthchecks/CustomHealthIndicator.java
@@ -1,20 +1,20 @@
-package com.springweb.healthchecks;
-
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CustomHealthIndicator implements HealthIndicator {
-
-	@Override
-	public Health health() {
-		boolean error = true;
-		if(error) {
-			return Health.down().withDetail("Error Key",123).build();
-		}
-
-		return Health.up().build();
-	}
-
-}
+//package com.springweb.healthchecks;
+//
+//import org.springframework.boot.actuate.health.Health;
+//import org.springframework.boot.actuate.health.HealthIndicator;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class CustomHealthIndicator implements HealthIndicator {
+//
+//	@Override
+//	public Health health() {
+//		boolean error = true;
+//		if(error) {
+//			return Health.down().withDetail("Error Key",123).build();
+//		}
+//
+//		return Health.up().build();
+//	}
+//
+//}
